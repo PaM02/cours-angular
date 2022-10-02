df46f7f5e73bd49f79e907025cc31581e77208d5
papa matar fall <matar026@gmail.com>
2022-10-02 19:34:31 +0000

    commite

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-face-snap.component.scss']
 })
 export class NewFaceSnapComponent implements OnInit {
+  snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.snapForm = this.formBuilder.group({
+      title: [null],
+      description: [null],
+      imageUrl: [null],
+      location: [null]
+    });
+
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+        ...formValue,
+        createdDate: new Date(),
+        snaps: 0,
+        id: 0
+      }))
+    );
+  }
+
+  onSubmitForm() {
+    console.log(this.snapForm.value);
   }
 
 }
